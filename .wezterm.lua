ef79e80ffa08bea006083f9396d80c529156aea2
kanokkorn <kanokorn@outlook.jp>
2026-08-16 12:30:09 +0700

    add wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,8 +7,14 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Everblush'
+-- config.color_scheme = 'Catppuccin Latte'
+config.color_scheme = 'Catppuccin Mocha'
+-- config.color_scheme = 'rose-pine'
+-- config.color_scheme = 'rose-pine-dawn'
+
 config.enable_tab_bar = false
+config.font_size = 12.0
+config.default_cursor_style = "SteadyBlock"
 
 -- and finally, return the configuration to wezterm
 return config
